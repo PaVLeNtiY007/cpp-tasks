53d49b2abe9bb966aa5975425eb1ddb69c2fb72d
Pavlo Romanuyk <pavlo.rom07.12@gmail.com>
2024-12-19 15:30:37 +0200

    4

---
+++ 4.1/4.1.cpp
@@ -0,0 +1,213 @@
+﻿#include <iostream>
+#include <stack>
+#include <unordered_map>
+#include <string>
+#include <vector>
+
+bool isValid(const std::string& s) {
+    std::stack<char> stack;
+    std::unordered_map<char, char> brackets = { {')', '('}, {'}', '{'}, {']', '['} };
+
+    for (char c : s) {
+        if (brackets.count(c)) {
+            if (stack.empty() || stack.top() != brackets[c]) {
+                return false;
+            }
+            stack.pop();
+        }
+        else {
+            stack.push(c);
+        }
+    }
+
+    return stack.empty();
+}
+
+
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+std::vector<int> inorderTraversal(TreeNode* root) {
+    std::vector<int> result;
+    std::stack<TreeNode*> stack;
+    TreeNode* current = root;
+
+    while (current || !stack.empty()) {
+        while (current) {
+            stack.push(current);
+            current = current->left;
+        }
+
+        current = stack.top();
+        stack.pop();
+        result.push_back(current->val);
+        current = current->right;
+    }
+
+    return result;
+}
+
+class MinStack {
+private:
+    std::stack<int> mainStack;
+    std::stack<int> minStack;
+
+public:
+    MinStack() {
+        minStack.push(INT_MAX);
+    }
+
+    void push(int val) {
+        mainStack.push(val);
+        minStack.push(std::min(val, minStack.top()));
+    }
+
+    void pop() {
+        mainStack.pop();
+        minStack.pop();
+    }
+
+    int top() {
+        return mainStack.top();
+    }
+
+    int getMin() {
+        return minStack.top();
+    }
+};
+
+
+
+class MyQueue {
+private:
+    std::stack<int> stack1, stack2;
+
+    void transfer() {
+        while (!stack1.empty()) {
+            stack2.push(stack1.top());
+            stack1.pop();
+        }
+    }
+
+public:
+    void push(int x) {
+        stack1.push(x);
+    }
+
+    int pop() {
+        if (stack2.empty()) {
+            transfer();
+        }
+        int result = stack2.top();
+        stack2.pop();
+        return result;
+    }
+
+    int peek() {
+        if (stack2.empty()) {
+            transfer();
+        }
+        return stack2.top();
+    }
+
+    bool empty() {
+        return stack1.empty() && stack2.empty();
+    }
+};
+
+
+
+std::string decodeString(const std::string& s) {
+    std::stack<std::string> strStack;
+    std::stack<int> numStack;
+    std::string currentStr;
+    int k = 0;
+
+    for (char c : s) {
+        if (isdigit(c)) {
+            k = k * 10 + (c - '0');
+        }
+        else if (c == '[') {
+            numStack.push(k);
+            strStack.push(currentStr);
+            currentStr = "";
+            k = 0;
+        }
+        else if (c == ']') {
+            std::string decoded = strStack.top();
+            strStack.pop();
+            int repeat = numStack.top();
+            numStack.pop();
+
+            for (int i = 0; i < repeat; ++i) {
+                decoded += currentStr;
+            }
+            currentStr = decoded;
+        }
+        else {
+            currentStr += c;
+        }
+    }
+
+    return currentStr;
+}
+
+
+
+int evalRPN(const std::vector<std::string>& tokens) {
+    std::stack<int> stack;
+
+    for (const std::string& token : tokens) {
+        if (token == "+" || token == "-" || token == "*" || token == "/") {
+            int b = stack.top();
+            stack.pop();
+            int a = stack.top();
+            stack.pop();
+
+            if (token == "+") stack.push(a + b);
+            else if (token == "-") stack.push(a - b);
+            else if (token == "*") stack.push(a * b);
+            else stack.push(a / b);
+        }
+        else {
+            stack.push(std::stoi(token));
+        }
+    }
+
+    return stack.top();
+}
+
+
+
+int longestValidParentheses(const std::string& s) {
+    std::stack<int> stack;
+    stack.push(-1);
+    int maxLength = 0;
+
+    for (int i = 0; i < s.length(); ++i) {
+        if (s[i] == '(') {
+            stack.push(i);
+        }
+        else {
+            stack.pop();
+            if (stack.empty()) {
+                stack.push(i);
+            }
+            else {
+                maxLength = std::max(maxLength, i - stack.top());
+            }
+        }
+    }
+
+    return maxLength;
+}
+
+int main()
+{
+    return 0;
+}
---
+++ 4.2/4.2.cpp
@@ -0,0 +1,203 @@
+﻿#include <iostream>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <deque>
+#include <queue>
+
+using namespace std;
+
+int firstUniqChar(string s) {
+    unordered_map<char, int> count;
+    for (char c : s)
+        count[c]++;
+    for (int i = 0; i < s.size(); i++)
+        if (count[s[i]] == 1)
+            return i;
+    return -1;
+}
+
+//
+
+class MyStack {
+    queue<int> q1, q2;
+public:
+    void push(int x) {
+        q2.push(x);
+        while (!q1.empty()) {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        swap(q1, q2);
+    }
+
+    int pop() {
+        int top = q1.front();
+        q1.pop();
+        return top;
+    }
+
+    int top() {
+        return q1.front();
+    }
+
+    bool empty() {
+        return q1.empty();
+    }
+};
+
+//
+
+class RecentCounter {
+    queue<int> q;
+public:
+    RecentCounter() {}
+
+    int ping(int t) {
+        q.push(t);
+        while (q.front() < t - 3000) {
+            q.pop();
+        }
+        return q.size();
+    }
+};
+
+//
+
+class MyCircularDeque {
+    vector<int> deque;
+    int front, rear, size, capacity;
+public:
+    MyCircularDeque(int k) : deque(k, 0), front(0), rear(k - 1), size(0), capacity(k) {}
+
+    bool insertFront(int value) {
+        if (isFull()) return false;
+        front = (front - 1 + capacity) % capacity;
+        deque[front] = value;
+        size++;
+        return true;
+    }
+
+    bool insertLast(int value) {
+        if (isFull()) return false;
+        rear = (rear + 1) % capacity;
+        deque[rear] = value;
+        size++;
+        return true;
+    }
+
+    bool deleteFront() {
+        if (isEmpty()) return false;
+        front = (front + 1) % capacity;
+        size--;
+        return true;
+    }
+
+    bool deleteLast() {
+        if (isEmpty()) return false;
+        rear = (rear - 1 + capacity) % capacity;
+        size--;
+        return true;
+    }
+
+    int getFront() {
+        return isEmpty() ? -1 : deque[front];
+    }
+
+    int getRear() {
+        return isEmpty() ? -1 : deque[rear];
+    }
+
+    bool isEmpty() {
+        return size == 0;
+    }
+
+    bool isFull() {
+        return size == capacity;
+    }
+};
+
+//
+
+vector<int> movesToStamp(string stamp, string target) {
+    int m = stamp.size(), n = target.size();
+    vector<int> res;
+    bool visited[n] = {};
+    int stars = 0;
+
+    while (stars < n) {
+        bool doneReplace = false;
+        for (int i = 0; i <= n - m; ++i) {
+            if (!visited[i] && canReplace(target, i, stamp)) {
+                stars = replace(target, i, m, stars);
+                visited[i] = true;
+                doneReplace = true;
+                res.push_back(i);
+                if (stars == n) break;
+            }
+        }
+        if (!doneReplace) return {};
+    }
+
+    reverse(res.begin(), res.end());
+    return res;
+}
+
+bool canReplace(string& target, int pos, string& stamp) {
+    for (int i = 0; i < stamp.size(); ++i) {
+        if (target[pos + i] != '?' && target[pos + i] != stamp[i])
+            return false;
+    }
+    return true;
+}
+
+int replace(string& target, int pos, int len, int count) {
+    for (int i = 0; i < len; ++i) {
+        if (target[pos + i] != '?') {
+            target[pos + i] = '?';
+            count++;
+        }
+    }
+    return count;
+}
+
+//
+
+vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+    deque<int> dq;
+    vector<int> res;
+    for (int i = 0; i < nums.size(); ++i) {
+        if (!dq.empty() && dq.front() == i - k)
+            dq.pop_front();
+        while (!dq.empty() && nums[dq.back()] < nums[i])
+            dq.pop_back();
+        dq.push_back(i);
+        if (i >= k - 1)
+            res.push_back(nums[dq.front()]);
+    }
+    return res;
+}
+
+//
+
+int constrainedSubsetSum(vector<int>& nums, int k) {
+    deque<int> dq;
+    int maxSum = nums[0];
+    for (int i = 0; i < nums.size(); ++i) {
+        nums[i] += dq.empty() ? 0 : max(0, nums[dq.front()]);
+        maxSum = max(maxSum, nums[i]);
+        while (!dq.empty() && nums[dq.back()] <= nums[i])
+            dq.pop_back();
+        dq.push_back(i);
+        if (dq.front() == i - k)
+            dq.pop_front();
+    }
+    return maxSum;
+}
+
+
+int main()
+{
+    return 0;
+}
